1ca6de87d20781f4baa793d74b6922b1d0e38efc
AaronxpiDev <aaronmcp66@gmail.com>
2026-04-01 00:21:59 -0600

    fix icons

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -26,7 +26,7 @@ class PaginationView extends View {
         <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
           <span>Page ${curPage + 1}</span>
           <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
+            <use xlink:href="${icons}#icon-arrow-right"></use>
           </svg>
         </button>
       `;
@@ -54,7 +54,7 @@ class PaginationView extends View {
         <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
           <span>Page ${curPage + 1}</span>
           <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
+            <use xlink:href="${icons}#icon-arrow-right"></use>
           </svg>
         </button>
       `;
